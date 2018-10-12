75bb159948f76365b34548ef1e0590ff774e2def
fczachor <fczachor@student.tgm.ac.at>
2018-10-12 12:16:49 +0200

    2nd Version

--- src/example1/example2.java
+++ src/example1/example2.java
@@ -3,5 +3,7 @@ package example1;
 public class example2 {
 	public static void main (String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
 	}
 }
